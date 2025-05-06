0f03b1a124676adc1a3b2e1a8c95db3f5a4a60d8
ulyildiz <ulashjob@gmail.com>
2025-05-06 12:56:31 +0300

    .

--- srcs/Containers/MariaDB/tools/mariadb_setup.sh
+++ srcs/Containers/MariaDB/tools/mariadb_setup.sh
@@ -6,11 +6,27 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 
-
 echo "${BLUE}1[MariaDB setup]${RESET}"
 
-# write down to terminal maridb config file
-echo "$(ls -la /etc/mysql/)"
-echo "$(cat /etc/mysql/conf.d)"
-echo "$(ls -la /etc/mysql/mariadb.conf.d/)"
-echo "$(cat /etc/mysql/mariadb.conf.d/50-server.cnf)"
+# initialize log directory
+if [ ! -d "/var/log/mysql" ]; then
+  echo "${YELLOW}Creating log directory...${RESET}"
+  mkdir -p /var/log/mysql
+fi
+chown -R mysql:mysql /var/log/mysql
+
+# initialize for socket
+if [ ! -d "/run/mysqld" ]; then
+  echo "${YELLOW}Creating mysqld run directory...${RESET}"
+  mkdir -p /run/mysqld
+fi
+chown -R mysql:mysql /run/mysqld
+
+# Initialize the database only if not already initialized
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+  echo "${YELLOW}Initializing MariaDB data directory...${RESET}"
+  mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+fi
+chown -R mysql:mysql /var/lib/mysql
+
+exec mariadbd
